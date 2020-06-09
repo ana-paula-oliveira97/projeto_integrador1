ee2ad916a95fc8373a689ad4024d25e9c64bafb2
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-06-09 08:18:01 -0300

    arquivos do projeto
    arquivos desenvolvidos do projeto ate agora

---
+++ js/formulario.js
@@ -0,0 +1,11 @@
+const nome = document.getElementById('nome')
+const serviso = document.getElementById('serviso')
+const semana = document.getElementById('semana')
+const horario = document.getElementById('horario')
+const telefone = document.getElementById('telefone')
+const comentario = document.getElementById('comentario')
+const resposta = document.getElementById('resposta')
+
+resposta.onclick = () =>{
+    const n = nome.nodeValue
+}
